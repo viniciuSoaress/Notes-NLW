6f6b050bb57e526a7fd0a67b87470f701f74c5ef
viniciszs <114860862+viniciszs@users.noreply.github.com>
2024-02-14 22:25:51 -0300

    add modal noteCard

--- src/app/components/note-card.tsx
+++ src/app/components/note-card.tsx
@@ -1,25 +1,56 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "./ui/dialog"
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from "date-fns/locale"
+
 
 type NoteCardProps = {
   note: {
     date: Date,
     content: string
   }
-    
+
 }
 
 export function NoteCard({ note }: NoteCardProps) {
 
   return (
-    <button className="rounded-md flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 text-left focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
-      <span className="text-sm font-medium text-slate-300">
-        {note.date.toISOString()}
-      </span>
+    <Dialog>
 
-      <p className="text-sm leading-6 text-slate-400">
-        {note.content}
-      </p>
+      <DialogTrigger className="rounded-md flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 text-left focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+        <span className="text-sm font-medium text-slate-300">
+          {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true})}
+        </span>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+        <p className="text-sm leading-6 text-slate-400">
+          {note.content}
+        </p>
+
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </DialogTrigger>
+
+      <DialogContent className="bg-slate-700 h-[60vh] pb-0">
+        <div className="flex flex-1 flex-col gap-3 p-5">
+          <span className="text-sm font-medium text-slate-300">
+            {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true})}
+          </span>
+
+          <p className="text-sm leading-6 text-slate-400">
+            {note.content}
+          </p>
+        </div>
+
+        <button
+         type="button"
+         className="w-full h-min bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none mt-72 rounded font-medium group"
+        >
+          Deseja <span className="text-red-400 group-hover:underline">apagar essa nota</span>?
+        </button>
+      </DialogContent>
+
+    </Dialog>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} dark`}>
+        {children}
+      </body>
     </html>
   );
 }
